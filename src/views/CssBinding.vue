43f458dddebc26cabed7a7d58887863e1cbf5e87
codewiz <c.chaiwat.ch@gmail.com>
2020-06-20 10:44:39 +0000

    Complete Form Input
    Signed-off-by: codewiz <c.chaiwat.ch@gmail.com>

---
+++ src/views/CssBinding.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="CssBinding">
+    <h1>Css Binding</h1>
+    <img :src="imgUrl" alt="vuemastery">
+    <br>
+    <div :class="[false ? error : '', textSize]">
+      CodWiz
+    </div>
+    <div :style="{color, fontSize}">Binding Style</div>
+    <div :style="isActive ? styleObj : ''">Binding style Obj</div>
+    <button @click="isActive = !isActive">ToggleActive</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CssBinding',
+  data() {
+    return {
+      imgUrl: 'https://www.vuemastery.com/images/vuemastery-white.svg',
+      error: 'errorClass',
+      textSize: 'textSize',
+      color: 'coral',
+      fontSize: '50px',
+      isActive: false,
+      styleObj: {
+        backgroundColor: 'yellow'
+      }
+    }
+  }
+
+}
+</script>
+
+<style>
+.errorClass {
+  background-color: tomato;
+}
+
+.textSize {
+  font-size: 20px;
+  font-weight: 900;
+}
+</style>
---
+++ src/views/formInput.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="formInput">
+    <h1>Form Input</h1>
+    <form @submit.prevent="submit">
+      <div>
+        <label for="first_name">FirstName: </label>
+        <input type="text" name="first_name" v-model="applicant.first_name">
+      </div> 
+      <div>
+        <label for="last_name">LastName: </label>
+        <input type="text" name="last_name" v-model="applicant.last_name">
+      </div> 
+      <!-- Sex -->
+      <div>
+        <label for="sex">Male</label>
+        <input type="radio" name="sex" value="male" v-model="applicant.sex">
+        <label for="sex">Female</label>
+        <input type="radio" name="sex" value="female" v-model="applicant.sex">
+      </div> 
+
+      <!-- Course -->
+      <div>
+        <select name="course" v-model="applicant.course">
+          <option value="react">React</option>
+          <option value="vue">Vue</option>
+          <option value="angular">Angular</option>
+        </select>
+      </div> 
+      <button type="button" @click="clear">Clear</button>
+      <button type="submit">Submit</button>
+    </form>
+    <div style="margin-top:15px; color:red;">
+      #Spy {{ JSON.stringify(applicant) }}
+    </div>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: "formInput",
+  data() {
+    return {
+      applicant: {
+        first_name: "",
+        last_name: "",
+        sex: "male",
+        course: "react"
+      }
+    }
+  },
+  methods: {
+    clear() {
+      this.applicant = {
+        first_name: "",
+        last_name: "",
+        sex: "male",
+        course: "react"
+      }
+    },
+    submit() {
+      alert(JSON.stringify(this.applicant))
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
